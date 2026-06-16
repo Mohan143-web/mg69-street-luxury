8b33694bc17a009289a40178146434e829b67bd4
Mohan Rajendran <159040492+Mohan143-web@users.noreply.github.com>
2026-06-15 22:05:29 -0400

    Import clean product gallery overrides

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
 import "./overlap-fixes.css";
+import "./product-gallery-clean.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>

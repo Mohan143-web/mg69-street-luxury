beb662e78dd038357dc2cef520a97bbe8dd0fc82
Mohan Rajendran <159040492+Mohan143-web@users.noreply.github.com>
2026-06-15 22:08:42 -0400

    Revert gallery css import to restore app

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
 import "./overlap-fixes.css";
-import "./product-gallery-clean.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
